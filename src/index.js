b5cebdf5f073acff95c0fdeb53ceda5175083ea3
inot65 <tft_toni@hotmail.com>
2023-11-21 00:05:55 +0200

    mai era ceva si in index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import {ContextProvider} from './context/Context';
-import {ToastContainer} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,6 +9,5 @@ root.render(
     <ContextProvider>
       <App />
     </ContextProvider>
-    <ToastContainer />
   </React.StrictMode>
 );
